c6332ad9504ae8ceed288deafd348720d6b60e10
Atticus Zambrana <atticus@kustomcakes.net>
2020-10-04 10:11:37 -0700

    oops accidentially leaked my token :flushed:

--- src/me/atticuszambrana/banana/Core.java
+++ src/me/atticuszambrana/banana/Core.java
@@ -6,7 +6,7 @@ public class Core {
 	public static Banana bananaBot;
 
 	public static void main(String[] args) {
-		bananaBot = new Banana("NzYxODQ1ODY5MzM4NDI3NDAz.X3giQQ.K90xK9zzAmr53KkK3iXzWpQyerc", ";");
+		bananaBot = new Banana(args[0], ";");
 	}
 
 }
